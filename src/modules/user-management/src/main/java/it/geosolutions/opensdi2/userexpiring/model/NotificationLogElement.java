6cc23493e69372931ab72b23fde744611e64f07e
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2014-07-04 15:49:21 +0200

    add user management module

---
+++ src/modules/user-management/src/main/java/it/geosolutions/opensdi2/userexpiring/model/NotificationLogElement.java
@@ -0,0 +1,69 @@
+/*
+ *  OpenSDI Manager
+ *  Copyright (C) 2014 GeoSolutions S.A.S.
+ *  http://www.geo-solutions.it
+ *
+ *  GPLv3 + Classpath exception
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * 
+ */
+package it.geosolutions.opensdi2.userexpiring.model;
+
+/**
+ * Bean to model the Notification Log
+ * 
+ * @author Lorenzo Natali (lorenzo.natali at geo-solutions.it)
+ * 
+ */
+public class NotificationLogElement {
+	private String receiver;
+	private String address;
+	private ExecutionState status;
+	private String message;
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public ExecutionState getStatus() {
+		return status;
+	}
+
+	public void setStatus(ExecutionState status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
---
+++ src/modules/user-management/src/main/java/it/geosolutions/opensdi2/userexpiring/model/UserExpiringStatus.java
@@ -0,0 +1,109 @@
+/*
+ *  OpenSDI Manager
+ *  Copyright (C) 2014 GeoSolutions S.A.S.
+ *  http://www.geo-solutions.it
+ *
+ *  GPLv3 + Classpath exception
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * 
+ */
+package it.geosolutions.opensdi2.userexpiring.model;
+
+import org.apache.commons.collections.buffer.CircularFifoBuffer;
+
+/**
+ * Modelling object for task execution status
+ * 
+ * @author Lorenzo Natali (lorenzo.natali at geo-solutions.it)
+ * 
+ */
+public class UserExpiringStatus {
+	private String lastRun;
+	private ExecutionState lastExecutionOutCome;
+	private String Message;
+	private TaskState status;
+	/**
+	 * The size of the log buffer
+	 */
+	private static final int BUFFER_SIZE = 30;
+	private CircularFifoBuffer expiredUsersLog = new CircularFifoBuffer(
+			BUFFER_SIZE);
+
+	/**
+	 * Add an element to the log
+	 * 
+	 * @param e
+	 */
+	public void addLog(ExpireLogElement e) {
+		expiredUsersLog.add(e);
+	}
+
+	public TaskState getStatus() {
+		return status;
+	}
+
+	public void setStatus(TaskState status) {
+		this.status = status;
+	}
+
+	public String getLastRun() {
+		return lastRun;
+	}
+
+	public void setLastRun(String lastRun) {
+		this.lastRun = lastRun;
+	}
+
+	public String getMessage() {
+		return Message;
+	}
+
+	public void setMessage(String message) {
+		Message = message;
+	}
+
+	/**
+	 * @return the lastExecutionEsit
+	 */
+	public ExecutionState getLastExecutionOutCome() {
+		return lastExecutionOutCome;
+	}
+
+	/**
+	 * @param lastExecutionEsit
+	 *            the lastExecutionEsit to set
+	 */
+	public void setLastExecutionOutCome(ExecutionState lastExecutionEsit) {
+		this.lastExecutionOutCome = lastExecutionEsit;
+	}
+
+	/**
+	 * @return the expiredUsersLog
+	 */
+	public CircularFifoBuffer getExpiredUsersLog() {
+		return expiredUsersLog;
+	}
+
+	/**
+	 * @param expiredUsersLog
+	 *            the expiredUsersLog to set
+	 */
+	public void setExpiredUsersLog(CircularFifoBuffer expiredUsersLog) {
+		this.expiredUsersLog = expiredUsersLog;
+	}
+
+}
