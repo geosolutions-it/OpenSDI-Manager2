b6f958b6eca9d4e2e3584484aef6f624f6300819
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2015-05-27 19:35:57 +0200

    port download service and c015 implementation

---
+++ src/modules/filemanager/src/main/java/it/geosolutions/opensdi2/download/ListDownloadService.java
@@ -0,0 +1,14 @@
+package it.geosolutions.opensdi2.download;
+
+import it.geosolutions.opensdi2.download.order.ListOrder;
+
+/**
+ * Order of type list
+ * @author Lorenzo Natali, GeoSolutions
+ *
+ */
+public interface ListDownloadService extends DownloadService<ListOrder>{
+
+	
+
+}
---
+++ src/modules/filemanager/src/main/java/it/geosolutions/opensdi2/download/OrderRegisterAware.java
@@ -0,0 +1,21 @@
+package it.geosolutions.opensdi2.download;
+
+import it.geosolutions.opensdi2.download.register.OrderRegistrer;
+/**
+ * Interface for a Order Register Aware class
+ * @author Lorenzo Natali, GeoSolutions
+ *
+ */
+public interface OrderRegisterAware  {
+	/**
+	 * Set the register
+	 * @param register
+	 */
+	void setOrderRegister(OrderRegistrer register);
+	
+	/**
+	 * get the register
+	 * @return the register
+	 */
+	OrderRegistrer getOrderRegister();
+}
